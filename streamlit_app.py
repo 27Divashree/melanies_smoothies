9b4ac3fec20fb92ea931c6f0dd4900165586846f
Divashree Agarwal <93401095+27Divashree@users.noreply.github.com>
2025-03-21 18:35:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,15 +5,15 @@ import requests
 import pandas as pd
 
 # Write directly to the app
-st.title("Example Streamlit App :cup_with_straw:")
+st.title("Customise your smoothies :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
 
 name_on_order = st.text_input('Name on Smoothie:')
-st.write('The name on your Smoothie wii be:',name_on_order)
+st.write('The name on your Smoothie will be:',name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 pd_df = my_dataframe.to_pandas()
